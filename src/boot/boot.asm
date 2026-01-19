b8f46b1424e2b0b3270a77c02d6571dc51306053
simone222222 <simone.paolini14@gmail.com>
2026-01-19 16:00:17 +0100

    Create boot.asm

---
+++ src/boot/boot.asm
@@ -0,0 +1,22 @@
+.set ALIGN,    1<<0
+.set MEMINFO,  1<<1
+.set FLAGS,    ALIGN | MEMINFO
+.set MAGIC,    0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
+.section .text
+.global _start
+.extern kernel_main
+
+_start:
+    call kernel_main
+    cli
+.hang:
+    hlt
+    jmp .hang
+    
